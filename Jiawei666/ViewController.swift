f02af37325f9aa05f40c574f23a1f26e499e502d
joewwwang <joewwwang@tencent.com>
2024-07-22 11:52:49 +0800

    Initial Commit

---
+++ Jiawei666/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Jiawei666
+//
+//  Created by 王嘉炜 on 2024/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
